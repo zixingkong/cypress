855cb4aaec0a52ce1857e2da57ae4516068a3a6d
Zach Bloomquist <github@chary.us>
2019-02-26 17:24:49 -0500

    socket: move built files to dist

--- packages/socket/index.js
+++ packages/socket/index.js
@@ -1,5 +1,6 @@
 if (process.env.CYPRESS_ENV !== 'production') {
   require('@packages/ts/register')
+  module.exports = require('./lib/socket')
+} else {
+  module.exports = require('./dist/lib/socket')
 }
-
-module.exports = require('./lib/socket')
