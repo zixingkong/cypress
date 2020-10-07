f3f37a8df322ed88dbe064fed1437ac35d6a9c8c
Jessica Sachs <jess@jessicasachs.io>
2020-10-07 18:02:17 -0400

    chore: adding release configuration base

--- npm/vue/.releaserc.js
+++ npm/vue/.releaserc.js
@@ -1,20 +1,7 @@
 module.exports = {
-  plugins: [
-    '@semantic-release/commit-analyzer',
-    '@semantic-release/release-notes-generator',
-  ],
-  extends: 'semantic-release-monorepo',
+  ...require('../../.releaserc.base'),
   branches: [
-    // '@cypress/vue@0.x',
-    // '@cypress/vue@1.0.0',
-    // { name: 'chore-prerelease-for-vue', channel: 'latest', prerelease: 'alpha' },
-    // 2.x.x-alpha
-    // { name: 'vue-3', prerelease: true, channel: '2.x.x-alpha' },
-    // 1.x.x-alpha
-    // { name: 'master', prerelease: true, channel: '1.x.x-alpha' },
+    '@cypress/vue@1.0.0',
+    { name: 'master', prerelease: 'alpha' },
   ],
 }
-
-// push
-// 1.0.0-alpha.1
-// 1.0.0-alpha.2
