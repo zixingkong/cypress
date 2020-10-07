809538a51a0e393408a7e371d5cba6341fd31aab
Jessica Sachs <jess@jessicasachs.io>
2020-10-07 16:00:16 -0400

    passing up semrel config

--- npm/vue/.releaserc.js
+++ npm/vue/.releaserc.js
@@ -1,5 +1,20 @@
 module.exports = {
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+  ],
+  extends: 'semantic-release-monorepo',
   branches: [
-    { name: 'chore-prerelease-for-vue', prerelease: true, channel: 'alpha' },
+    // '@cypress/vue@0.x',
+    // '@cypress/vue@1.0.0',
+    // { name: 'chore-prerelease-for-vue', channel: 'latest', prerelease: 'alpha' },
+    // 2.x.x-alpha
+    // { name: 'vue-3', prerelease: true, channel: '2.x.x-alpha' },
+    // 1.x.x-alpha
+    // { name: 'master', prerelease: true, channel: '1.x.x-alpha' },
   ],
 }
+
+// push
+// 1.0.0-alpha.1
+// 1.0.0-alpha.2
